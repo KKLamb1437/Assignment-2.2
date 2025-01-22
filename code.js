509d054ea05bdf75f3fb24badbd43cdc234e829b
KKLamb1437 <KLAMB81023@uat.edu>
2025-01-21 23:05:35 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+function updateText(){
+    document.getElementById("heading").innerHTML = "Let's play Craps!";
+}
+function random(){
+           
+   document.getElementById("Craps").innerHTML = Math.floor(Math.random() * 10) + 2; 
+    
+   } 
+
